212f463c7a36359232415c68127b27b8a41e4538
hongsam14 <ghdtjdgus14@gmail.com>
2024-04-26 01:06:19 +0900

    dev env

---
+++ go.mod
@@ -0,0 +1,3 @@
+module my_little_Sysmon
+
+go 1.22.2
---
+++ kernel/kernel.h
@@ -0,0 +1,15 @@
+#ifndef KERNEL_H
+# define KERNEL_H
+
+#include <stddef.h>
+#include <string.h>
+
+#include <linux/limits.h>
+#include <linux/bpf.h>
+#include <linux/sched.h>
+
+#include <bpf/bpf_helpers.h>
+#include <bpf/bpf_helper_defs.h>
+
+
+#endif
